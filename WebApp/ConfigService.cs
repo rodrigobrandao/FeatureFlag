49d13ae1728b02e9d3c8fc39491dab68e9e7ac4b
Rodrigo <Rodrigo@DESKTOP-EU1NRKM>
2016-12-18 21:38:36 -0200

    WebApp created

---
+++ WebApp/ConfigService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class ConfigService : IConfigService
+    {
+        public FeatureFlag.IConfiguration FeatureFlag { get; set; }
+
+        public ConfigService()
+        {
+            this.FeatureFlag = global::FeatureFlag.Features.GetConfiguration();
+        }
+    }
+
+    public interface IConfigService
+    {
+        FeatureFlag.IConfiguration FeatureFlag { get; set; }
+
+        
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfigService ConfigService { get; set; }
+
+        public HomeController(IConfigService configService)
+        {
+            this.ConfigService = configService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (this.ConfigService.FeatureFlag.IsAvailable("Feature02"))
+            {
+                ViewBag.Message = "Feature02 is available";
+            }
+            else
+            {
+                ViewBag.Message = "Feature02 is not available";
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
